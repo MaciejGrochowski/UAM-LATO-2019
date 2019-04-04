d4bda8bf8583b534239b898deecbfef28a1472c6
software-wizard <wieczorek.przemyslaw@outlook.com>
2019-04-04 10:43:24 +0200

    X

--- src/main/java/pl/wizard/software/creatures/Creature.java
+++ src/main/java/pl/wizard/software/creatures/Creature.java
@@ -1,4 +1,31 @@
 package pl.wizard.software.creatures;
 
 class Creature {
+
+    private final String name;
+    private final int maxHp;
+    private int currentHp;
+    private final int attack;
+    private final int defence;
+
+    Creature(String aName, int aMaxHp, int aAttack, int aDefence) {
+        name = aName;
+        maxHp = aMaxHp;
+        currentHp = maxHp;
+        attack = aAttack;
+        defence = aDefence;
+    }
+
+    void attack(Creature aDefender) {
+        int damageToDeal = this.attack - aDefender.defence;
+        if (damageToDeal > 0) {
+            aDefender.currentHp -= damageToDeal;
+        } else {
+            aDefender.currentHp -= 1;
+        }
+    }
+
+    int getCurrentHp() {
+        return currentHp;
+    }
 }
--- src/test/java/pl/wizard/software/creatures/CreatureTest.java
+++ src/test/java/pl/wizard/software/creatures/CreatureTest.java
@@ -7,14 +7,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreatureTest {
 
     @Test
-    void centaurAttackImp(){
-        Creature centaur = new Creature();
-        Creature imp = new Creature();
+    void impShouldLostTwoHpAfterCentaurAttack(){
+        Creature centaur = parepareCentaur();
+        Creature imp = prepareImp();
 
         centaur.attack(imp);
 
         assertEquals(2, imp.getCurrentHp());
+    }
 
+    @Test
+    void centaurShouldLostOnlyOneHpBecauseHisDefenseIsGreaterThanImpAttack(){
+        Creature centaur = parepareCentaur();
+        Creature imp = prepareImp();
+
+        imp.attack(centaur);
+
+        assertEquals(9, centaur.getCurrentHp());
+    }
+
+    @Test
+    void impShouldCounterAttack(){
+        Creature centaur = parepareCentaur();
+        Creature imp = prepareImp();
+
+        imp.attack(centaur);
+
+        //then
+        assertEquals(9, centaur.getCurrentHp());
+        assertEquals(2, imp.getCurrentHp());
+    }
+
+    private Creature prepareImp() {
+        return new Creature("Imp",4,2,3);
+    }
+
+    private Creature parepareCentaur() {
+        return new Creature("Centaur", 10,5,3);
     }
 
 }

64b227b7d6deace584a1a8de93888bfc89d406f6
software-wizard <wieczorek.przemyslaw@outlook.com>
2019-04-04 10:46:10 +0200

    X

--- src/test/java/pl/wizard/software/creatures/CreatureTest.java
+++ src/test/java/pl/wizard/software/creatures/CreatureTest.java
@@ -1,16 +1,33 @@
 package pl.wizard.software.creatures;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreatureTest {
 
+    Creature centaur;
+    Creature imp;
+
+
+    @BeforeEach
+    void prepareCreatures(){
+        centaur = prepareCentaur();
+        imp = prepareImp();
+    }
+
+    private Creature prepareImp() {
+        return new Creature("Imp",4,2,3);
+    }
+
+    private Creature prepareCentaur() {
+        return new Creature("Centaur", 10,5,3);
+    }
+
+
     @Test
     void impShouldLostTwoHpAfterCentaurAttack(){
-        Creature centaur = parepareCentaur();
-        Creature imp = prepareImp();
-
         centaur.attack(imp);
 
         assertEquals(2, imp.getCurrentHp());
@@ -18,9 +35,6 @@ class CreatureTest {
 
     @Test
     void centaurShouldLostOnlyOneHpBecauseHisDefenseIsGreaterThanImpAttack(){
-        Creature centaur = parepareCentaur();
-        Creature imp = prepareImp();
-
         imp.attack(centaur);
 
         assertEquals(9, centaur.getCurrentHp());
@@ -28,22 +42,11 @@ class CreatureTest {
 
     @Test
     void impShouldCounterAttack(){
-        Creature centaur = parepareCentaur();
-        Creature imp = prepareImp();
-
         imp.attack(centaur);
 
         //then
         assertEquals(9, centaur.getCurrentHp());
         assertEquals(2, imp.getCurrentHp());
-    }
-
-    private Creature prepareImp() {
-        return new Creature("Imp",4,2,3);
-    }
-
-    private Creature parepareCentaur() {
-        return new Creature("Centaur", 10,5,3);
     }
 
 }

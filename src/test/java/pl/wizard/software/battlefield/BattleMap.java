e81571e75fb8d973d88612c90c7d09474442743c
software-wizard <wieczorek.przemyslaw@outlook.com>
2019-04-18 10:38:50 +0200

    X

--- src/test/java/pl/wizard/software/battlefield/BattleMap.java
+++ src/test/java/pl/wizard/software/battlefield/BattleMap.java
@@ -3,14 +3,22 @@ package pl.wizard.software.battlefield;
 import pl.wizard.software.creatures.Creature;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class BattleMap {
+
+    Map<Creature, Point> map = new HashMap<>();
+
     void put(Creature aCreature, Point aPoint) {
+        map.put(aCreature, aPoint);
     }
 
     void move(Creature aCreature, Point aPoint) {
+        map.replace(aCreature, aPoint);
     }
 
     Point getCreaturePosition(Creature aCreature) {
+        return map.get(aCreature);
     }
 }

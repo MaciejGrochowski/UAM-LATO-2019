710b2a0b4aa20224514839ee705c4622199ed361
software-wizard <wieczorek.przemyslaw@outlook.com>
2019-04-22 05:12:13 +0200

    X

--- src/main/java/pl/wizard/software/battlefield/BattleMap.java
+++ src/main/java/pl/wizard/software/battlefield/BattleMap.java
@@ -1,5 +1,6 @@
 package pl.wizard.software.battlefield;
 
+import com.google.common.collect.Range;
 import pl.wizard.software.creatures.Creature;
 
 import java.awt.*;
@@ -10,11 +11,22 @@ class BattleMap {
 
     static final int MAX_WIDTH = 15;
     static final int MAX_HEIGHT = 10;
-    private final Map<Creature, Point> map = new HashMap<>();
+    private final Map<Creature, Point> map;
+    private final Range<Integer> width;
+    private final Range<Integer> height;
+
+    BattleMap(){
+        map = new HashMap<>();
+        width = Range.closed(0,MAX_WIDTH);
+        height = Range.closed(0,MAX_HEIGHT);
+    }
 
     void put(Creature aCreature, Point aPoint) {
         if (map.containsValue(aPoint)) {
             throw new IllegalArgumentException("Field is not empty");
+        }
+        if(!width.contains((int)aPoint.getX()) || !height.contains((int)aPoint.getY())){
+            throw new IllegalArgumentException("You are trying to add creature outside the map!");
         }
         map.put(aCreature, aPoint);
     }

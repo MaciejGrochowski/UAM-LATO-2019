212697f32c27bed4e6cc3da32fa7c3f471c5d1de
software-wizard <przemek@software-wizard.pl>
2019-04-24 05:30:12 +0200

    X

--- src/main/java/pl/wizard/software/player/Hero.java
+++ src/main/java/pl/wizard/software/player/Hero.java
@@ -2,8 +2,25 @@ package pl.wizard.software.player;
 
 import pl.wizard.software.creatures.Creature;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Hero {
 
-    void addCreature(Creature aCreature_1) {
+    private final List<Creature> creatures;
+
+    Hero(){
+        creatures = new ArrayList<>();
+    }
+
+    Hero(List<Creature> aCreatures) {
+        creatures = aCreatures;
+    }
+
+    void addCreature(Creature aCreature) {
+        if(creatures.size()>=5){
+            throw new IllegalStateException("Hero doesn't have empty slot for next creature");
+        }
+        creatures.add(aCreature);
     }
 }

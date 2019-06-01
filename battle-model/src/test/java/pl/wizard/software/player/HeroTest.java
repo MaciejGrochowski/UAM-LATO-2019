4ddfbe2d9132a6198317fe927165eb4fefc66b73
Maciej Grochowski <macgro1@st.amu.edu.pl>
2019-06-01 13:59:55 +0200

    Tested hero_attack_mechanism

--- battle-model/src/test/java/pl/wizard/software/player/HeroTest.java
+++ battle-model/src/test/java/pl/wizard/software/player/HeroTest.java
@@ -16,15 +16,17 @@ class HeroTest {
     private Creature creature1;
     private Creature creature2;
     private Creature creature3;
+    private Hero hero3;
 
     @BeforeEach
     void prepareHeroes(){
 
         hero1 = new Hero(2,2,2,2);
         hero2 = new Hero(0,0,0,0);
+        hero3 = new Hero(0, 5, 0, 0);
         creature1 = new Creature("1", 10, Range.closed(1,2), 0, new DefaultDamageStrategy(), NOT_IMPORTANT, hero1, 1);
         creature2 = new Creature("2", 2, Range.closed(1,2), 0, new DefaultDamageStrategy(), NOT_IMPORTANT, hero2, 1);
-        creature3 = new Creature("3", 5, Range.closed(2,2), 0, new DefaultDamageStrategy(), NOT_IMPORTANT, hero1, 1);
+        creature3 = new Creature("3", 20, Range.closed(2,2), 0, new DefaultDamageStrategy(), NOT_IMPORTANT, hero3, 1);
     }
 
 
@@ -42,17 +44,27 @@ class HeroTest {
     }
 
     @Test
-    void CreatureshoulddieBecauseOfHerosStats1(){
+    void creatureShouldDieBecauseOfHerosStats(){
         creature1.attack(creature2);
-        assertTrue(!creature2.isAlive());
+        assertFalse(creature2.isAlive());
     }
 
     @Test
-    void creatureShouldntDieBecauseOfHerosStats2(){
+    void creatureShouldntDieBecauseOfHerosStats(){
         creature1.setHero(hero2);
         creature2.setHero(hero1);
         creature1.attack(creature2);
         assertTrue(creature2.isAlive());
+    }
+
+    @Test
+    void creatureShouldDieAfter20Attacks(){
+        for(int i=0;i<19;i++){
+            creature1.attack(creature3);
+        }
+        assertTrue(creature3.isAlive());
+        creature1.attack(creature3);
+        assertFalse(creature3.isAlive());
     }
 
 }
